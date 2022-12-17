3ef024dd4d60448637f7d6f68f9331b657ee218a
gyeom0919 <leegyeom09@gmail.com>
2022-12-18 05:59:39 +0900

    컴마 수정
    ㅋ

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -5,7 +5,7 @@ let dependencies = Dependencies(
         .remote(url: "https://github.com/ReactiveX/RxSwift.git", requirement: .upToNextMajor(from: "6.5.0")),
         .remote(url: "https://github.com/SnapKit/SnapKit.git", requirement: .upToNextMajor(from: "5.6.0")),
         .remote(url: "https://github.com/devxoul/Then", requirement: .upToNextMajor(from: "3.0.0")),
-        .remote(url: "https://github.com/AliSoftware/Reusable.git", requirement: .upToNextMajor(from: "4.1.2"))
+        .remote(url: "https://github.com/AliSoftware/Reusable.git", requirement: .upToNextMajor(from: "4.1.2")),
         .remote(url: "https://github.com/airbnb/lottie-ios.git", requirement: .upToNextMajor(from: "4.0.1"))
     ]
 )
