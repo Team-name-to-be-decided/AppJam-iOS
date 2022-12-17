3eb7216f9158ee0aeb8bc5ece0ea3a05f1bb30c6
baegteun <baegteun@gmail.com>
2022-12-18 05:48:16 +0900

    :bento: :: Nearby image

--- App/Sources/App/Scenes/Card/CardViewController.swift
+++ App/Sources/App/Scenes/Card/CardViewController.swift
@@ -1,5 +1,23 @@
 import UIKit
 
 final class CardViewController: BaseViewController {
-    
+    private let searchingImageView = UIImageView(image: AJICon.connect.image.withRenderingMode(.alwaysOriginal))
+    private let searchingLabel = UILabel().then {
+        $0.numberOfLines = 2
+        $0.text = "주변 기기를 탐색하고 있습니다...\n잠시만 기다려주세요"
+        $0.font = .init(font: AJFont.regular, size: 15)
+        $0.textColor = AJColor.veryDarkGray.color
+    }
+    override func addView() {
+        view.addSubviews(searchingImageView, searchingLabel)
+    }
+    override func setLayout() {
+        searchingImageView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+        searchingLabel.snp.makeConstraints {
+            $0.centerX.equalTo(searchingImageView)
+            $0.top.equalTo(searchingImageView.snp.centerY).offset(75)
+        }
+    }
 }
